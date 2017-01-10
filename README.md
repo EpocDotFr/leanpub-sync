68537acfe8aa9373119869e05280e3fbc0521a87
Epoc <contact@epoc.fr>
2017-01-10 23:26:18 +0100

    Heeey

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Leanpub library synchronization
 
-Tired to manually download every single books from your [Leanpub](https://leanpub.com/) library? I got you covered!
+Tired to manually download every single book from your [Leanpub](https://leanpub.com/) library to your e-reader? I got you covered.
+
+## Prerequisites
+
+Python 3. May eventually works with Python 2 (not tested).
+
+## Installation
+
+Clone this repo, and then the usual `pip install -r requirements.txt`.
+
+## Configuration
+
+Copy the `.env.example` file to `.env` and fill in the configuration parameters.
+
+Available configuration parameters are:
+
+  - `LEANPUB_EMAIL` Leanpub account email
+  - `LEANPUB_PASSWORD` Leanpub account password
+  - `LEANPUB_PREFERED_FORMAT` Format of the books to download. May be `epub` (for reading on phones and tablets), `pdf` (for reading on a computer) or `mobi` (for reading on a Kindle). Will automatically download an alternative version if your prefered isn't available
+  - `OUTPUT_DIR` Directory in which to download the books (relative or absolute)
+
+## Usage
+
+```
+python run.py
+```
+
+## How it works
+
+Please refer to the script itself (`run.py`), it is self-explanatory.
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from envparse import env, Env
+from lxml import html
+import requests
 import logging
-import os
+import sys
 
 
 def debug(message, err=False, terminate=False):
@@ -8,7 +10,7 @@ def debug(message, err=False, terminate=False):
     logging.getLogger().log(logging.ERROR if err else logging.INFO, message)
 
     if terminate:
-		sys.exit(1)
+        sys.exit(1)
 
 
 def run():
@@ -18,9 +20,47 @@ def run():
         stream=sys.stdout
     )
 
-	logging.getLogger().setLevel(logging.INFO)
+    logging.getLogger().setLevel(logging.INFO)
 
+    Env.read_envfile('.env')
 
+    # Firstly, we'll get the token called "authenticity_token" on the login page (https://leanpub.com/login),
+    # stored in a hidden input. This token - which is always different - is required when submitting the login form.
+
+    logging.info('Getting the authenticity token')
+
+    login_page = requests.get('https://leanpub.com/login').content
+
+    login_page_parsed = html.fromstring(login_page)
+
+    authenticity_token = login_page_parsed.xpath('//input[@name="authenticity_token"]/@value')[0]
+
+    logging.info('Authenticity token: ' + authenticity_token)
+
+    # Next, we'll forge the data to be sent to the login endpoint (https://leanpub.com/session) using the token we
+    # scraped above, along your credentials.
+
+    login_data = {
+        'utf8': '✓',
+        'session[email]': env('LEANPUB_EMAIL'),
+        'session[password]': env('LEANPUB_PASSWORD'),
+        'authenticity_token': authenticity_token
+    }
+
+    login_result = requests.post('https://leanpub.com/session', data=login_data)
+
+    login_result.raise_for_status()
+
+    logging.info('Logged in successfully')
+
+    # We are now ready to get the list of books to download from the library. Fortunately, there's - at least on the
+    # library page - a JSON-formatted API which simplify the process to extract them.
+
+    logging.info('Getting books to download')
+
+    purchased_packages = requests.get('https://leanpub.com/api/v1/purchased_packages?include=book').json
+
+    # https://leanpub.com/api/v1/purchased_packages?include=book%2C%20book.accepted_authors&archived=false&page=1&sort=created_at_desc&type=library&user_id=223538
 
 if __name__ == '__main__':
-	run()
+    run()
